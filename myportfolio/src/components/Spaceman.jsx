f721c79e6b9ba014e4b2661325a910d2be766ac3
LEXES7 <sachinthabhashitha245@gmail.com>
2025-04-16 16:21:40 +0530

    spaceman update

--- myportfolio/src/components/Spaceman.jsx
+++ myportfolio/src/components/Spaceman.jsx
@@ -68,7 +68,7 @@ const SpacemanCanvas = () => {
         <directionalLight position={[1, 1, 1]} intensity={2} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 5, 10]} intensity={2} />
-        <Spaceman scale={[1.5, 1.5, 1.5]} position={[0, 1.5, 0]} />
+        <Spaceman scale={[1.5, 1.5, 1.5]} position={[0, 0, 0]} />
       </Suspense>
     </Canvas>
   );
